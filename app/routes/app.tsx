066904dbf459ef1e04e8c4c6d80204378a983f71
beeve111 <39862732+beeve111@users.noreply.github.com>
2026-01-04 14:40:55 +0000

    Updated the embed logic

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,7 +1,8 @@
 import type { LoaderFunctionArgs } from "react-router";
-import { Outlet, useLoaderData, useRouteError } from "react-router";
+import { Link, Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
+import { NavMenu } from "@shopify/app-bridge-react"; // <--- IMPORT FROM HERE
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -15,11 +16,13 @@ export default function App() {
 
   return (
     <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/welcome">Welcome</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
-      </s-app-nav>
+      <NavMenu>
+        {/* The first link must have rel="home" */}
+        <Link to="/app" rel="home">Home</Link>
+        <Link to="/app/welcome">Welcome</Link>
+        <Link to="/app/additional">Additional page</Link>
+      </NavMenu>
+      
       <Outlet />
     </AppProvider>
   );
